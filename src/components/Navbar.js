bc1712284bd26900bf7dc5d474e6f3568284e59e
Ale Kennedy <alepertuz@gmail.com>
2023-08-23 12:39:12 -0400

    pages and components setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link as ScrollLink } from 'react-scroll';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Eventdocs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Eventdocs = () => {
+  return (
+    <div>Eventdocs</div>
+  )
+}
+
+export default Eventdocs
