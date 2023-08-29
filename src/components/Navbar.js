7963c337370aea254dd472474270ae46e8ffb3a6
Ale Kennedy <alepertuz@gmail.com>
2023-08-29 12:19:30 -0400

    Navbar element complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Link } from "react-router-dom";
 import { useState } from "react";
+import { LuX, LuMenu } from "react-icons/lu";
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
@@ -44,7 +46,7 @@ const Navbar = () => {
     ];
 
     return (
-        <nav className="bg-zinc-100">
+        <nav className="h-auto bg-fuchsia-100">
             <div className="px-4 mx-auto sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
 
@@ -53,7 +55,7 @@ const Navbar = () => {
                            {navlinks.map((link, index) => (
                               <a
                                  key={index}
-                                 className="px-3 py-2 font-medium text-gray-300 rounded hover:bg-gray-600 hover:text-white-md text-md"
+                                 className="px-3 py-2 font-medium text-purple-300 transition-all duration-500 rounded hover:bg-purple-400 hover:text-white-md text-md"
                                  href={link.link}
                               >
                                  {link.title}
@@ -67,64 +69,36 @@ const Navbar = () => {
                             src="images/cleargblogo.png"
                             alt="Logo"
                         />
-                        <h1 className="text-2xl font-medium">GoBridge</h1>
+                        <h1 className="text-2xl font-medium text-purple-700">GoBridge</h1>
+                    </div>
+                    <div className="flex mr-2 md:hidden">
+                        <button type = "button" onClick={toggleMenu} className="inline-flex items-center justify-center p-2 text-purple-400 bg-purple-700 rounded-md outline-none hover:text-white hover:bg-purple-700 focus:outline-none focus: ring-2 focus:ring-offset-2 focus:ring-offset-purple-800 focus:ring-white"
+                        >
+                           <span className="sr-only"> Open Main Menu</span>
+                           {menuOpen === true ? <LuX/> : <LuMenu />}
+                        </button>
                     </div>
                 </div>
             </div>
+            {menuOpen ? (
+               <div className="md:hidden">
+                  <div className="pt-2 pb-3 space-y-1 ox-2 sm:px-3">
+                     {navlinks.map((link, index) => (
+                              <a
+                                 key={index}
+                                 className="block px-3 py-2 text-base font-medium text-purple-400 rounded-md hover:bg-gray-700 hover:text-white"
+                                 href={link.link}
+                              >
+                                 {link.title}
+                              </a>
+                     ))}
+                     
+                  </div>
+                  
+               </div>
+            ) : null }
         </nav>
     );
 };
 
 export default Navbar;
-
-/* <div className='relative'>
-<button
-   className='text-gray-600 md:hidden hover:text-gray-800 focus:outline-none'
-   onClick={toggleMenu}
->
-   <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='w-6 h-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
-   >
-      <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      strokeWidth='2'
-      d='M4 6h16M4 12h16m-7 6h7'
-      />
-   </svg>
-</button>
-   <div className={`absolute md:hidden top-10 right-0 ${menuOpen ? 'block' : 'hidden'}`}>
-      <div className='px-4 py-2 bg-white shadow-lg'>
-         <BrowserRouter>
-         <Link className='block mb-2' to='/Home'>
-            Home
-         </Link>
-         <Link className='block mb-2' to='/Initiatives'>
-            Current Initiatives
-         </Link>
-         <Link className='block mb-2' to='/Talks'>
-            Give a Talk
-         </Link>
-         <Link className='block mb-2' to='/Communities'>
-            Communities
-         </Link>
-         <Link className='block mb-2' to='/Workshops'>
-            Organize a Workshop
-         </Link>
-         <Link className='block mb-2' to='/Eventdocs'>
-            Important Event Documents
-         </Link>
-         <Link className='block mb-2' to='/Sponsor'>
-            Become a Sponsor
-         </Link>
-         <Link className='block' to='/Donate'>
-            Donate
-         </Link>
-         </BrowserRouter>
-      </div>
-   </div>
-</div> */
