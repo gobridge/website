c46906c1eeb70b1c791cbc0aaa2b44b3880d5dcd
William Kennedy <bill@ardanlabs.com>
2023-10-04 13:29:00 -0400

    better startup and shutdown code

--- app/services/website/main.go
+++ app/services/website/main.go
@@ -1,35 +1,137 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"regexp"
+	"syscall"
+	"time"
+
+	"github.com/ardanlabs/conf/v3"
+	"github.com/gobridge/website/foundation/logger"
 )
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Println(err)
+	var log *logger.Logger
+
+	traceIDFunc := func(ctx context.Context) string {
+		return ""
+	}
+
+	log = logger.New(os.Stdout, logger.LevelInfo, "WEBSITE", traceIDFunc)
+
+	// -------------------------------------------------------------------------
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Info(ctx, "ERROR", "msg", err)
 		os.Exit(1)
 	}
 }
 
-func run() error {
+func run(ctx context.Context, log *logger.Logger) error {
+
+	// -------------------------------------------------------------------------
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			Host            string        `conf:"default:0.0.0.0:3000"`
+		}
+	}{
+		Version: conf.Version{
+			Desc: "GoBridge Website",
+		},
+	}
+
+	const prefix = ""
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service")
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config", out)
+
+	// -------------------------------------------------------------------------
+	// Start API Service
+
+	log.Info(ctx, "startup", "status", "starting website")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	api := http.Server{
+		Addr:         cfg.Web.Host,
+		Handler:      http.DefaultServeMux,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		ErrorLog:     logger.NewStdLogger(log, logger.LevelError),
+	}
+
+	// -------------------------------------------------------------------------
+
 	fileServer := http.FileServer(http.Dir("app/services/website/static"))
 	fileMatcher := regexp.MustCompile(`\.[a-zA-Z]*$`)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if !fileMatcher.MatchString(r.URL.Path) {
+			log.Info(ctx, "request", "url", r.URL.Path)
 			http.ServeFile(w, r, "app/services/website/static/index.html")
 		} else {
 			fileServer.ServeHTTP(w, r)
 		}
 	})
 
-	log.Print("Listening on 0.0.0.0:3000...")
-	err := http.ListenAndServe("0.0.0.0:3000", nil)
-	if err != nil {
-		return err
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Info(ctx, "startup", "status", "api router started", "host", api.Addr)
+
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// -------------------------------------------------------------------------
+	// Shutdown
+
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+
+	case sig := <-shutdown:
+		log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(ctx, cfg.Web.ShutdownTimeout)
+		defer cancel()
+
+		if err := api.Shutdown(ctx); err != nil {
+			api.Close()
+			return fmt.Errorf("could not stop server gracefully: %w", err)
+		}
 	}
 
 	return nil
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gobridge/website
 
 go 1.21.1
+
+require github.com/ardanlabs/conf/v3 v3.1.6
